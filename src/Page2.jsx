b9b9219d338170ab602619193c2d1f6d73aaa906
zorro901 <zoro907@gmail.com>
2021-08-08 20:01:56 +0900

    22. クエリパラメータを扱う

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -5,6 +5,8 @@ export const Page2 = () => {
     <div>
       <h1>Page2ページです</h1>
       <Link to="/page2/999">URL Parameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>UrlParameterページです</h1>
       <p>a {id}</p>
+      <p>クエリパラメータは{query.get("name")}</p>
     </div>
   );
 };
