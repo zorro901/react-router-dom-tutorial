bf4e76590db73456485f575621d4b05713bd21d8
zorro901 <zoro907@gmail.com>
2021-08-08 18:17:12 +0900

    Initial commit

---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>Page1ページです</h1>
+    </div>
+  );
+};
---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>Page2ページです</h1>
+    </div>
+  );
+};
