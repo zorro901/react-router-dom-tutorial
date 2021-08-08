364d0fd063c97e24d6d11feb7f24d1edf275439e
zorro901 <zoro907@gmail.com>
2021-08-08 19:12:16 +0900

    19. ネストされたページ遷移

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -10,17 +12,31 @@ export default function App() {
       <div className="App">
         <Link to="/"> Home </Link>
         <br />
-        <Link to="/Page1"> Page1 </Link>
+        <Link to="/page1"> Page1 </Link>
         <br />
-        <Link to="/Page2"> Page2 </Link>
+        <Link to="/page2"> Page2 </Link>
       </div>
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              {console.log(url)}
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
